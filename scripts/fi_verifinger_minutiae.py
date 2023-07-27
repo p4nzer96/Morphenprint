d7c14988d5623560045a493dc365cf6b9fd83deb
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2023-07-27 16:21:05 +0200

    Added logical changes to fetch verifinger minutiae

--- scripts/fi_verifinger_minutiae.py
+++ scripts/fi_verifinger_minutiae.py
@@ -3,15 +3,44 @@ import os
 import traceback
 from subprocess import check_output
 
-def get_verifinger_minutiae(img_path, minutiae_save_path_name):
+
+def get_verifinger_minutiae(img_path, minutiae_save_path):
     try:
         # get the response
-        lines = check_output(['VerifingerMinutiae', img_path])
+        out_lines = check_output(['VerifingerMinutiae', img_path])
+        lines = out_lines.split(b'\n');
 
-        # Save minutiae to a text file
-        with open(minutiae_save_path_name, 'w') as file:
+        # Save minutiae in bytes to a text file
+        with open(minutiae_save_path, 'w', encoding='utf-8') as file:
             for line in lines:
                 file.write(f"{line}\n")
+        
+        # variables
+        modified_lines = []
+        extracted_lines = []
+        start_index = 0
+        end_index = 0
+        
+        # Read from saved minutiae file and extract only minutia points
+        with open(minutiae_save_path, 'r') as file:
+            for index, line in enumerate(file, 1):
+                try:
+                    if line.startswith('b'):
+                        line = line[1:]
+                        line = line.replace("'", "")
+                        if line.startswith('#minutiae'):
+                            start_index = index
+                        if line.startswith('deltasCount'):
+                            end_index = index
+                    modified_lines.append(line)
+                except:
+                    continue
+            
+        if (start_index != 0 and end_index != 0):
+            extracted_lines = modified_lines[start_index : end_index - 1]
+
+        with open(minutiae_save_path, 'w') as file:
+            file.writelines(extracted_lines)
 
     except Exception as e:
         print('Error in fetching verfinger minutiae -' + str(e))
@@ -25,8 +54,8 @@ def main():
         try:   
             img1_cropped_path = ''
             img2_cropped_path = ''
-            img1_minutiae_save_name = ''
-            img2_minutiae_save_name = ''
+            img1_minutiae_save_path = ''
+            img2_minutiae_save_path = ''
             count = 0
             for file in files:
                 # Check if the file is an image
@@ -38,11 +67,11 @@ def main():
                         img2_cropped_path = os.path.join(root, file)
             
             if (img1_cropped_path and img2_cropped_path):
-                img1_minutiae_save_name = str(os.path.splitext(os.path.basename(img1_cropped_path))[0]) + '_minutiae.txt'
-                get_verifinger_minutiae(img1_cropped_path, img1_minutiae_save_name)
+                img1_minutiae_save_path = root + '/' + str(os.path.splitext(os.path.basename(img1_cropped_path))[0]) + '_minutiae.txt'
+                get_verifinger_minutiae(img1_cropped_path, img1_minutiae_save_path)
 
-                img2_minutiae_save_name = str(os.path.splitext(os.path.basename(img2_cropped_path))[0]) + '_minutiae.txt'
-                get_verifinger_minutiae(img2_cropped_path, img2_minutiae_save_name)
+                img2_minutiae_save_path = root + '/' + str(os.path.splitext(os.path.basename(img2_cropped_path))[0]) + '_minutiae.txt'
+                get_verifinger_minutiae(img2_cropped_path, img2_minutiae_save_path)
         
         except Exception as e:
             print('Error -' + str(e))
